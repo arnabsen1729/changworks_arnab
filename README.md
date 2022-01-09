93f57918a598b27c1687c7b28a12702774568afd
Arnab Sen <arnabsen1729@gmail.com>
2022-01-09 19:50:43 +0530

    add logic to handle the word
    There is a counter state which keeps increasing as the user presses
    changworks correctly. If there is any incorrect key press it gets reset
    to zero, also incase the user successfully types the word it will again
    reset to zero.

--- README.md
+++ README.md
@@ -6,7 +6,13 @@
 
 ## Instructions
 
+You need to have `node` and `npm` installed, in your machine. Follow this [docs](https://docs.npmjs.com/downloading-and-installing-node-js-and-npm). If you already have it, you can move ahead.
+
 To run the project locally, follow these steps:
+
+### `npm install`
+
+This will install all the packages needed for this application to run.
 
 ### `npm start`
 
--- src/components/typer/Typer.js
+++ src/components/typer/Typer.js
@@ -1,4 +1,5 @@
 import { GlobalHotKeys } from 'react-hotkeys';
+import { useState, useEffect } from 'react';
 import './Typer.css';
 
 const alpha = Array.from(Array(26)).map((e, i) => i + 97);
@@ -7,15 +8,41 @@ const lowercase = alpha.map((x) => String.fromCharCode(x));
 const keyMap = {
     ALPHABET_PRESS: { sequences: lowercase, action: 'keydown' },
 };
-const handlers = { ALPHABET_PRESS: (event) => console.log(event) };
+
+const target = 'changworks';
 
 function Typer() {
+    const [currentKey, setCurrentKey] = useState('');
+    const [targetIndex, setTargetIndex] = useState(0);
+
+    const handleKeyPress = (key) => {
+        setCurrentKey(key);
+    };
+
+    const handlers = {
+        ALPHABET_PRESS: (event) => handleKeyPress(event.key),
+    };
+
+    useEffect(() => {
+        if (targetIndex === target.length) {
+            // TODO: handle cofetti
+            setTargetIndex(0);
+        }
+    }, [targetIndex]);
+
+    useEffect(() => {
+        setTargetIndex((t) => {
+            if (currentKey === target[t]) return t + 1;
+            else return 0;
+        });
+    }, [currentKey]);
+
     return (
         <>
             <GlobalHotKeys keyMap={keyMap} handlers={handlers} />
             <div className="typer">
                 <div className="typer-text">Type "changworks"</div>
-                <div className="typer-keypress">keypress</div>
+                <div className="typer-keypress">{targetIndex}</div>
             </div>
         </>
     );
