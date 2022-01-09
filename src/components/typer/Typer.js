37c94f4862d0a79dcd9d9936635a5931598f22f8
Arnab Sen <arnabsen1729@gmail.com>
2022-01-09 13:38:21 +0530

    react-hotkeys integrated
    Now, we can type any alphabet and it log the corresponding event in the
    console.

--- src/components/typer/Typer.js
+++ src/components/typer/Typer.js
@@ -1,11 +1,23 @@
+import { GlobalHotKeys } from 'react-hotkeys';
 import './Typer.css';
+
+const alpha = Array.from(Array(26)).map((e, i) => i + 97);
+const lowercase = alpha.map((x) => String.fromCharCode(x));
+
+const keyMap = {
+    ALPHABET_PRESS: { sequences: lowercase, action: 'keydown' },
+};
+const handlers = { ALPHABET_PRESS: (event) => console.log(event) };
 
 function Typer() {
     return (
-        <div className="typer">
-            <div className="typer-text">Type "changworks"</div>
-            <div className="typer-keypress">keypress</div>
-        </div>
+        <>
+            <GlobalHotKeys keyMap={keyMap} handlers={handlers} />
+            <div className="typer">
+                <div className="typer-text">Type "changworks"</div>
+                <div className="typer-keypress">keypress</div>
+            </div>
+        </>
     );
 }
 
