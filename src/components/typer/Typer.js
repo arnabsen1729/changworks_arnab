0a920deb1deaa36e654f2145a5a1c8493845ad37
Arnab Sen <arnabsen1729@gmail.com>
2022-01-09 12:09:46 +0530

    basic layout of the application

---
+++ src/components/typer/Typer.js
@@ -0,0 +1,12 @@
+import './Typer.css';
+
+function Typer() {
+    return (
+        <div className="typer">
+            <div className="typer-text">Type "changworks"</div>
+            <div className="typer-keypress">keypress</div>
+        </div>
+    );
+}
+
+export default Typer;
